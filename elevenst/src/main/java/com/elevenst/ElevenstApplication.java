c577f1b75a51ea440882ed2ce30757b9cc2d4eca
Jangsukwoo <wwwkdtjrdn@naver.com>
2020-10-18 20:45:31 +0900

    xml_Parsing

---
+++ elevenst/src/main/java/com/elevenst/ElevenstApplication.java
@@ -0,0 +1,13 @@
+package com.elevenst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevenstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevenstApplication.class, args);
+	}
+
+}
---
+++ elevenst/src/test/java/com/elevenst/ElevenstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevenst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevenstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
